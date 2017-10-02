4b94b7d0afbb3c62d4c9fc714161067f1e8ed2aa
Maxim <thanatormax@gmail.com>
2017-10-03 00:20:21 +0300

    Добавил болванки классов

---
+++ Lab1/InfoTable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class InfoTable
+    {
+        //Табличные поля
+        //Ничего кроме конструктора не надо
+    }
+}
---
+++ Lab1/Method.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Method
+    {
+        Point currP;
+        private int a; //параметр пропорциональности а
+        private int maxsteps;//максимальное количество шагов
+        private double T; //начальная температура
+        private double h; //шаг
+        private double eps; //контроль шага
+        private double eBorder; //контроль выхода на границу
+        //Вообще ещё нужно, чтобы в ходе работы метода он выводил сразу в таблицу необходимые данные
+        //Также нужен список точек для графика
+        //Плюс нужны поля, которые будут в таблице
+        public void Init(Point _currP, int _a, int _maxsteps, int _T, double _h, double _eps, double eBorder)
+        {
+
+        }
+
+        public bool NeedStop()
+        {
+            return false;
+        }
+    }
+}
